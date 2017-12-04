055c41adb342599cdffbb002b6f285f60f817320
Thomas Lovén <thomasloven@gmail.com>
2017-12-04 01:30:18 +0100

    Scrolling screen when necessary

--- src/kernel/drivers/vga.c
+++ src/kernel/drivers/vga.c
@@ -2,22 +2,41 @@
 #include <memory.h>
 
 void *vidmem;
-struct {
-  uint8_t character;
-  uint8_t format;
-}__attribute__((packed)) buffer[VGA_SIZE];
+struct vga_cell{
+  uint8_t c;
+  uint8_t f;
+}__attribute__((packed));
+
+struct vga_cell buffer[VGA_SIZE];
 
 uint64_t cursor;
+uint8_t format;
 
 void vga_init()
 {
   vidmem = VGA_MEMORY;
   memset(vidmem, 0, VGA_SIZE*2);
+
+  format = 0x7;
 }
 
 void flush()
 {
   memcpy(vidmem, buffer, sizeof(buffer));
+}
+
+void scroll()
+{
+  while(cursor >= VGA_SIZE)
+  {
+    for(int i = 0; i < VGA_ROWS-1; i++)
+      for(int j = 0; j < VGA_COLS; j++)
+        buffer[VGA_POS(i, j)] = buffer[VGA_POS(i+1, j)];
+    for(int i = 0; i < VGA_COLS; i++)
+      buffer[VGA_POS(VGA_ROWS-1, i)] = (struct vga_cell){.c='\0', .f=format};
+  cursor -= VGA_COLS;
+  }
+
 }
 
 void vga_write(char c)
@@ -29,9 +48,9 @@ void vga_write(char c)
       cursor -= VGA_COL(cursor);
       break;
     default:
-      buffer[cursor].character = c;
-      buffer[cursor].format = 0x7;
+      buffer[cursor] = (struct vga_cell){.c = c, .f=format};
       cursor++;
   }
+  scroll();
   flush();
 }
