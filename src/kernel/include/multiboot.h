2451851b9fc49d8986c17edf0791af4d5d361d72
Thomas Lovén <thomasloven@gmail.com>
2017-12-07 22:47:52 +0100

    Remove Multiboot1 support

--- src/kernel/include/multiboot.h
+++ src/kernel/include/multiboot.h
@@ -1,12 +1,7 @@
 #pragma once
 
-#define MBOOT1_MAGIC 0x1BADB002
-#define MBOOT1_REPLY 0x2BADB002
-#define MBOOT1_FLAGS 0
-#define MBOOT1_CS -(MBOOT1_FLAGS + MBOOT1_MAGIC)
-
 #define MBOOT2_MAGIC 0xE85250D6
 #define MBOOT2_REPLY 0x36D76289
 #define MBOOT2_ARCH 0
-#define MBOOT2_LENGTH (MultibootHeader2End - MultibootHeader2)
+#define MBOOT2_LENGTH (Multiboot2HeaderEnd - Multiboot2Header)
 #define MBOOT2_CHECKSUM -(MBOOT2_MAGIC + MBOOT2_ARCH + MBOOT2_LENGTH)
