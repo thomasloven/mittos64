1daf8ed47bf4aee6cef1697e810a5afd861be29e
Thomas Lovén <thomasloven@gmail.com>
2017-12-21 16:15:55 +0100

    PMM - alloc two pages

--- src/kernel/memory/pmm.c
+++ src/kernel/memory/pmm.c
@@ -4,12 +4,13 @@ uintptr_t *first = 0;
 
 void pmm_free(void *c)
 {
+  *(uintptr_t *)c = (uintptr_t)first;
   first = c;
 }
 
 void *pmm_alloc()
 {
   void *c = first;
-  first = 0;
+  first = (uintptr_t *)(c?*(uintptr_t *)c:0);
   return c;
 }
