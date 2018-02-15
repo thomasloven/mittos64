607266bfd37c303deae187a5cd39514dffc1d659
Thomas Lovén <thomasloven@gmail.com>
2018-02-15 10:50:03 +0100

    Clean up interface

--- src/kernel/include/multiboot.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MBOOT2_MAGIC 0xE85250D6
-#define MBOOT2_REPLY 0x36D76289
-#define MBOOT2_ARCH 0
-#define MBOOT2_LENGTH (Multiboot2HeaderEnd - Multiboot2Header)
-#define MBOOT2_CHECKSUM -(MBOOT2_MAGIC + MBOOT2_ARCH + MBOOT2_LENGTH)
